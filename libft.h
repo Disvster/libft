2b69431c76d8d305df251b83353a48aa437c989c
Disvster <mmcouto2001@gmail.com>
2024-02-06 19:15:13 +0000

    added funtions to .h

--- libft.h
+++ libft.h
@@ -18,4 +18,19 @@
 # include <stdio.h>
 # include <string.h>
 
+size_t	ft_strlen(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_atoi(const char *str);
+int	ft_strncmp(char *s1, char *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, size_t n);
+
+
 #endif
