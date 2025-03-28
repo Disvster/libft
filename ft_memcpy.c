1aa0ce634802863e01da4323194aa40858d40a97
Disvster <mmcouto2001@gmail.com>
2025-03-28 16:49:27 +0000

    changed memcopy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,11 +15,11 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t		i;
-	const char	*source;
-	const char	desti;
+	unsigned char	*source;
+	unsigned char	*desti;
 
-	source = (const char *)source;
-	desti = (const char *)dest;
+	source = (unsigned char *)source;
+	desti = (unsigned char *)dest;
 	i = -1;
 	if (!desti && !source)
 		return (0);
