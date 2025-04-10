e67e797bfcb6d7d1461d5decd8fdf44d90e4da7e
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-10 20:24:40 +0100

    added Makefile and callocWIP, corrected isascii

---
+++ Makefile
@@ -0,0 +1,25 @@
+SRCS = ft_memmove.c ft_atoi.c ft_isalpha.c ft_isdigit.c ft_memcmp.c ft_memchr.c ft_isalnum.c ft_isascii.c ft_strlen.c ft_memcpy.c ft_toupper.c ft_strlcat.c ft_isprint.c ft_strrchr.c ft_strncmp.c ft_strnstr.c ft_strchr.c tester_fts.c ft_calloc.c ft_memset.c ft_bzero.c ft_tolower.c ft_strlcpy.c
+OBJS = $(SRCS:.c=.o)
+CC = cc
+CFLAGS = -Wall -Werror -Wextra -c
+NAME = libft.a
+ARRCS = ar rcs
+RM = rm -f
+
+.c.o:
+	$(CC) $(CFLAGS) $< -o $(<:.c=.o)
+
+$(NAME): $(OBJS)
+	$(ARRCS) $(NAME) $(OBJS)
+
+all: $(NAME)
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- ft_calloc.c
+++ ft_calloc.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/08 14:19:23 by manmaria          #+#    #+#             */
-/*   Updated: 2025/04/10 20:17:47 by manmaria         ###   ########.fr       */
+/*   Created: 2025/04/10 19:18:43 by manmaria          #+#    #+#             */
+/*   Updated: 2025/04/10 19:49:01 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isascii(int c)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	return (c >= 0 && c <= 127);
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	tmp = (char *)malloc(size * nmemb);
+	tmp = NULL;
+	if (((size * nmemb) > (size_t)-1) || !nmemb || !size)
+		return (tmp);
+	while (i < nmemb)
+		tmp[i++] = 0;
+	return (tmp);
 }
