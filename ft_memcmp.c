64441c3a696420f44c1619b1cb2dd2a13e3f0931
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-10 17:10:22 +0100

    norm corrected

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:45:33 by manmaria          #+#    #+#             */
-/*   Updated: 2025/04/10 17:01:19 by manmaria         ###   ########.fr       */
+/*   Updated: 2025/04/10 17:08:59 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*p_s1;
 	unsigned char	*p_s2;
+	size_t			i;
 
+	i = 0;
 	p_s1 = (unsigned char *)s1;
 	p_s2 = (unsigned char *)s2;
-	//while (n)
-	return (0);	
+	while (i < n)
+	{
+		if (p_s1[i] != p_s2[i])
+			return (p_s1[i] - p_s2[i]);
+		i++;
+	}
+	return (0);
 }
+
 /*{
 	if (!n)
 		return (0);
