cfb9239c4985aa0d71cc02417c6a1459ec16670e
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-16 15:42:53 +0100

    finished strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,17 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
- NOTE:
- Applies the function f to each character of the
- string s, passing its index as the first argument
- and the character itself as the second. A new
- string is created (using malloc(3)) to store the
- results from the successive applications of f.
-  
-  RETURN is the string created from the successive applications
-   of ’f’.*/
-//                              The function to apply to each character.
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char			*mapi;
